63dd65e83d431d7ad048ff5dc0df9d257d32e099
Automated MSK Jenkins User <msk_jenkins@mskbuildhost-u1604.desy.de>
2018-03-26 05:53:39 +0200

    Created/updated docu for QtHardMon master (automatic commit)

--- QtHardMon/master/class_register_type_abstractor.js
+++ QtHardMon/master/class_register_type_abstractor.js
@@ -5,8 +5,11 @@ var class_register_type_abstractor =
     [ "isIntegral", "class_register_type_abstractor.html#a60cb6d79dac7d07c72182a97071dbea3", null ],
     [ "nChannels", "class_register_type_abstractor.html#ac7cec671b0756136f127f0fb66f055dd", null ],
     [ "nElements", "class_register_type_abstractor.html#a72d33cece14afed7d5ee356e02d26c0b", null ],
+    [ "rawData", "class_register_type_abstractor.html#abe40109c68aacae04b546e28e67d1093", null ],
+    [ "rawDataAsHex", "class_register_type_abstractor.html#af8f97fe27d17eb03a865d9105f8336b8", null ],
     [ "rawDataType", "class_register_type_abstractor.html#a4c7f3d727786422b84e11f856420f165", null ],
     [ "read", "class_register_type_abstractor.html#a8b382ff726fdbb372383e107bb543936", null ],
     [ "setData", "class_register_type_abstractor.html#a8b68c0521e05262667d6a02f40871d9d", null ],
+    [ "setRawData", "class_register_type_abstractor.html#a478cd5890a491bafdb1d500dafdeda0f", null ],
     [ "write", "class_register_type_abstractor.html#a8cfd3ba37f751b16192916318bb9519e", null ]
 ];
