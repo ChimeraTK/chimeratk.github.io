23877b9edd60e9334af448a04d6a5db0c135cc7c
Automated MSK Jenkins User <msk_jenkins@mskbuildhost-u1604.desy.de>
2018-06-26 05:53:18 +0200

    Created/updated docu for DeviceAccess-DoocsBackend master (automatic commit)

--- DeviceAccess-DoocsBackend/master/search/all_6.js
+++ DeviceAccess-DoocsBackend/master/search/all_6.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['getaccessmodeflags',['getAccessModeFlags',['../classmtca4u_1_1_doocs_backend_register_accessor.html#a1261acaded8e0cb8d6244623204604a1',1,'mtca4u::DoocsBackendRegisterAccessor']]],
   ['gethardwareaccessingelements',['getHardwareAccessingElements',['../classmtca4u_1_1_doocs_backend_register_accessor.html#a9842474b14d587a51384dbbff53676a6',1,'mtca4u::DoocsBackendRegisterAccessor']]],
   ['getinternalelements',['getInternalElements',['../classmtca4u_1_1_doocs_backend_register_accessor.html#a048bfc4a3fdef74d942a3445276150ad',1,'mtca4u::DoocsBackendRegisterAccessor']]],
   ['getregisteraccessor_5fimpl',['getRegisterAccessor_impl',['../classmtca4u_1_1_doocs_backend.html#aea6ef533f6acaebecfce2e9a648c9639',1,'mtca4u::DoocsBackend']]],
