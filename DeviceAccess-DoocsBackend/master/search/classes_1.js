959824303de8dd5af5c866f16e896dba05b40663
Automated MSK Jenkins User <msk_jenkins@msk-ubuntu1604.desy.de>
2018-08-30 07:53:21 +0200

    Created/updated docu for DeviceAccess-DoocsBackend master (automatic commit)

--- DeviceAccess-DoocsBackend/master/search/classes_1.js
+++ DeviceAccess-DoocsBackend/master/search/classes_1.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['doocsbackend',['DoocsBackend',['../classmtca4u_1_1_doocs_backend.html',1,'mtca4u']]],
   ['doocsbackendfloatregisteraccessor',['DoocsBackendFloatRegisterAccessor',['../classmtca4u_1_1_doocs_backend_float_register_accessor.html',1,'mtca4u']]],
+  ['doocsbackendiiiiregisteraccessor',['DoocsBackendIIIIRegisterAccessor',['../classmtca4u_1_1_doocs_backend_i_i_i_i_register_accessor.html',1,'mtca4u']]],
   ['doocsbackendintregisteraccessor',['DoocsBackendIntRegisterAccessor',['../classmtca4u_1_1_doocs_backend_int_register_accessor.html',1,'mtca4u']]],
   ['doocsbackendlongregisteraccessor',['DoocsBackendLongRegisterAccessor',['../classmtca4u_1_1_doocs_backend_long_register_accessor.html',1,'mtca4u']]],
   ['doocsbackendregisteraccessor',['DoocsBackendRegisterAccessor',['../classmtca4u_1_1_doocs_backend_register_accessor.html',1,'mtca4u']]],
