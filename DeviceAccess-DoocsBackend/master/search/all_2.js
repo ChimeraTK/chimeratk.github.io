89bbf7ef520f521d9671e9424b62e9c637b609a3
Automated MSK Jenkins User <msk_jenkins@msk-ubuntu1604.desy.de>
2018-09-15 07:53:40 +0200

    Created/updated docu for DeviceAccess-DoocsBackend master (automatic commit)

--- DeviceAccess-DoocsBackend/master/search/all_2.js
+++ DeviceAccess-DoocsBackend/master/search/all_2.js
@@ -4,5 +4,5 @@ var searchData=
   ['catalogueobtained',['catalogueObtained',['../class_chimera_t_k_1_1_doocs_backend.html#acc99fe024803ee394205e02ce7d9d242',1,'ChimeraTK::DoocsBackend']]],
   ['chimeratk',['ChimeraTK',['../namespace_chimera_t_k.html',1,'']]],
   ['close',['close',['../class_chimera_t_k_1_1_doocs_backend.html#aa0b104650a279c02f853d7a7f6e07f48',1,'ChimeraTK::DoocsBackend']]],
-  ['createinstance',['createInstance',['../class_chimera_t_k_1_1_doocs_backend.html#af673fbf154067e6c78945470a3f816f4',1,'ChimeraTK::DoocsBackend']]]
+  ['createinstance',['createInstance',['../class_chimera_t_k_1_1_doocs_backend.html#a6858ec1a1833cc69444017506e68fb62',1,'ChimeraTK::DoocsBackend']]]
 ];
