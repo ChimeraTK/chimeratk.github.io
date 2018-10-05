b498dd0cc9ca8ba7c80f702f818c31fe00125a70
Automated MSK Jenkins User <msk_jenkins@msk-ubuntu1604.desy.de>
2018-10-05 07:58:08 +0200

    Created/updated docu for MotorDriverCard 01.08.00 (automatic commit)

---
+++ MotorDriverCard/01.08/search/defines_1.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['card_5fconfig_5fadd_5fparameter',['CARD_CONFIG_ADD_PARAMETER',['../_motor_driver_card_config_x_m_l_8cc.html#a9367062f4d773642e1592a2df32d831c',1,'MotorDriverCardConfigXML.cc']]],
+  ['card_5fconfig_5fadd_5fregister',['CARD_CONFIG_ADD_REGISTER',['../_motor_driver_card_config_x_m_l_8cc.html#a21ff40de53aebebb12953355c6a4daf9',1,'MotorDriverCardConfigXML.cc']]],
+  ['controler_5fconfig_5fadd_5fparameter',['CONTROLER_CONFIG_ADD_PARAMETER',['../_motor_driver_card_config_x_m_l_8cc.html#a0f4e21c4a17553934a5943e470b92be0',1,'MotorDriverCardConfigXML.cc']]],
+  ['controler_5fconfig_5fadd_5fregister',['CONTROLER_CONFIG_ADD_REGISTER',['../_motor_driver_card_config_x_m_l_8cc.html#a1c780a6fb1a5d98eee33693c9afe8b58',1,'MotorDriverCardConfigXML.cc']]]
+];
---
+++ MotorDriverCard/01.08/search/variables_e.js
@@ -0,0 +1,20 @@
+var searchData=
+[
+  ['size_5fof_5fspi_5faddress_5fspace',['SIZE_OF_SPI_ADDRESS_SPACE',['../namespacemtca4u_1_1tmc260.html#a03e796d17e0a2c4c7809943128d70ec6',1,'mtca4u::tmc260::SIZE_OF_SPI_ADDRESS_SPACE()'],['../namespacemtca4u_1_1tmc429.html#ab8c639e2e020a1484d40bd57afe374e4',1,'mtca4u::tmc429::SIZE_OF_SPI_ADDRESS_SPACE()']]],
+  ['smda_5fcommon',['SMDA_COMMON',['../namespacemtca4u_1_1tmc429.html#ade4fc4acec5fd297b6141fcdc133899d',1,'mtca4u::tmc429']]],
+  ['spi_5fdata_5fmask',['SPI_DATA_MASK',['../namespacemtca4u_1_1tmc429.html#a1c54be883a5e26939dde72a617187bf9',1,'mtca4u::tmc429']]],
+  ['spi_5fdefault_5fwaiting_5ftime',['SPI_DEFAULT_WAITING_TIME',['../classmtca4u_1_1_s_p_ivia_p_c_ie.html#a0b12fea46722b0d69f116e90265b9273',1,'mtca4u::SPIviaPCIe']]],
+  ['spi_5fsync_5ferror',['SPI_SYNC_ERROR',['../namespacemtca4u_1_1dfmc__md22.html#abf4d7b5e9b8ef8cc59f1217ae4cee011',1,'mtca4u::dfmc_md22']]],
+  ['spi_5fsync_5fok',['SPI_SYNC_OK',['../namespacemtca4u_1_1dfmc__md22.html#aff86ce421b77c9ec4919ffea8e9c79b1',1,'mtca4u::dfmc_md22']]],
+  ['spi_5fsync_5frequested',['SPI_SYNC_REQUESTED',['../namespacemtca4u_1_1dfmc__md22.html#a41c3584b3c2981e1fbda848eaebf5351',1,'mtca4u::dfmc_md22']]],
+  ['spi_5fsync_5fsuffix',['SPI_SYNC_SUFFIX',['../namespacemtca4u_1_1dfmc__md22.html#ae2bbf6e9bdcdfb241810089b815d3820',1,'mtca4u::dfmc_md22']]],
+  ['spi_5fwrite_5fsuffix',['SPI_WRITE_SUFFIX',['../namespacemtca4u_1_1dfmc__md22.html#aaf80f7218ce55b69f27008aa67710e22',1,'mtca4u::dfmc_md22']]],
+  ['stall_5fguard_5fcontrol_5fdefault',['STALL_GUARD_CONTROL_DEFAULT',['../namespacemtca4u.html#a35b84ebad0b6ba69023bc6fe0a34cd32',1,'mtca4u']]],
+  ['stall_5fguard_5fvalue_5fsuffix',['STALL_GUARD_VALUE_SUFFIX',['../namespacemtca4u_1_1dfmc__md22.html#ae4fc1caecb9f843ce5ca3a02e5349906',1,'mtca4u::dfmc_md22']]],
+  ['stallguardcontroldata',['stallGuardControlData',['../structmtca4u_1_1_motor_controler_config.html#ae36a3360d9d6a79fb6495da2ed67babc',1,'mtca4u::MotorControlerConfig']]],
+  ['status',['status',['../classmtca4u_1_1_linear_stepper_motor_status_and_error.html#a28702952d7bfd6a02270281da85408be',1,'mtca4u::LinearStepperMotorStatusAndError::status()'],['../classmtca4u_1_1_stepper_motor_status_and_error.html#a0fc5e626108150c92cbe459371f659f4',1,'mtca4u::StepperMotorStatusAndError::status()']]],
+  ['status_5fsuffix',['STATUS_SUFFIX',['../namespacemtca4u_1_1dfmc__md22.html#ab5e87f171c099d32cbaab14c12d61c08',1,'mtca4u::dfmc_md22']]],
+  ['stepper_5fmotor_5fglobal_5fparameters_5fdefault',['STEPPER_MOTOR_GLOBAL_PARAMETERS_DEFAULT',['../namespacemtca4u.html#a79c25f27e80a4a75c32b46ae7357f510',1,'mtca4u']]],
+  ['steppermotorglobalparameters',['stepperMotorGlobalParameters',['../structmtca4u_1_1_motor_driver_card_config.html#a90f5e3c50c3e7ec7394574427ee36cb0',1,'mtca4u::MotorDriverCardConfig']]],
+  ['stopevent',['stopEvent',['../class_chimera_t_k_1_1_stepper_motor_state_machine.html#aad107b170a71aad6be657029fe059129',1,'ChimeraTK::StepperMotorStateMachine']]]
+];
