516720c1990c8347780af12950f60c2885502332
Automated MSK Jenkins User <msk_jenkins@msk-ubuntu1604.desy.de>
2018-12-23 06:53:13 +0100

    Created/updated docu for DeviceAccess master (automatic commit)

--- DeviceAccess/master/search/variables_3.js
+++ DeviceAccess/master/search/variables_3.js
@@ -6,6 +6,7 @@ var searchData=
   ['channel',['channel',['../class_chimera_t_k_1_1_l_n_m_backend_register_info.html#aff42f74c45e0976b23e7842469af46da',1,'ChimeraTK::LNMBackendRegisterInfo']]],
   ['cmd',['cmd',['../structdevice__ioctrl__data.html#abce7358be06d7ce7e756fe02c25c187e',1,'device_ioctrl_data']]],
   ['copydecorators',['copyDecorators',['../class_chimera_t_k_1_1_transfer_group.html#a8f35ead3c8f95c0415a9a8d83b48d463',1,'ChimeraTK::TransferGroup']]],
+  ['createdataconverter_3c_20ieee754_5fsingleconverter_20_3e',['createDataConverter&lt; IEEE754_SingleConverter &gt;',['../namespace_chimera_t_k_1_1detail.html#a3043a9633f9257450574c8882af34621',1,'ChimeraTK::detail']]],
   ['creatormap',['creatorMap',['../class_chimera_t_k_1_1_backend_factory.html#a64ecc91ff9ce1c132a06ca0700699daf',1,'ChimeraTK::BackendFactory']]],
   ['creatormap_5fcompat',['creatorMap_compat',['../class_chimera_t_k_1_1_backend_factory.html#a6b455a60d105b5e2fe71a29aa9c8ae78',1,'ChimeraTK::BackendFactory']]],
   ['currentmodule',['currentModule',['../class_chimera_t_k_1_1_logical_name_map_parser.html#a4a022ca1c826353be4053b35049fa510',1,'ChimeraTK::LogicalNameMapParser']]]
