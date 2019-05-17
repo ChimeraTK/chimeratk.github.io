954ca297712d2873f349f7f6631cad3dda7a681e
Automated MSK Jenkins User <msk_jenkins@msk-ubuntu1604.desy.de>
2019-05-17 03:53:42 +0000

    Created/updated docu for ControlSystemAdapter-DoocsAdapter 00.10.01 (automatic commit)

--- ControlSystemAdapter-DoocsAdapter/00.10/_doocs_p_v_factory_8cc.js
+++ ControlSystemAdapter-DoocsAdapter/00.10/_doocs_p_v_factory_8cc.js
@@ -1,5 +1,5 @@
 var _doocs_p_v_factory_8cc =
 [
-    [ "DoocsPVFactory::createDoocsScalar< std::string, D_string >", "_doocs_p_v_factory_8cc.html#adbbbb15994bdc83f36aa4cefd5e124d7", null ],
+    [ "DoocsPVFactory::createDoocsScalar< std::string, D_text >", "_doocs_p_v_factory_8cc.html#a79cb56aed1eab42a7908f4a5119bbe03", null ],
     [ "DoocsPVFactory::typedCreateDoocsArray< std::nullptr_t, std::nullptr_t >", "_doocs_p_v_factory_8cc.html#a457018846c1d386363e87ed20b9d9c79", null ]
 ];
--- ControlSystemAdapter-DoocsAdapter/00.10/search/classes_2.js
+++ ControlSystemAdapter-DoocsAdapter/00.10/search/classes_2.js
@@ -3,6 +3,7 @@ var searchData=
   ['doocsadapter',['DoocsAdapter',['../class_chimera_t_k_1_1_doocs_adapter.html',1,'ChimeraTK']]],
   ['doocsprocessarray',['DoocsProcessArray',['../class_chimera_t_k_1_1_doocs_process_array.html',1,'ChimeraTK']]],
   ['doocsprocessscalar',['DoocsProcessScalar',['../class_chimera_t_k_1_1_doocs_process_scalar.html',1,'ChimeraTK']]],
+  ['doocsprocessscalar_3c_20t_2c_20d_5ftext_20_3e',['DoocsProcessScalar&lt; T, D_text &gt;',['../class_chimera_t_k_1_1_doocs_process_scalar_3_01_t_00_01_d__text_01_4.html',1,'ChimeraTK']]],
   ['doocspvfactory',['DoocsPVFactory',['../class_chimera_t_k_1_1_doocs_p_v_factory.html',1,'ChimeraTK']]],
   ['doocsspectrum',['DoocsSpectrum',['../class_chimera_t_k_1_1_doocs_spectrum.html',1,'ChimeraTK']]],
   ['doocstransferelement',['DoocsTransferElement',['../class_chimera_t_k_1_1_doocs_transfer_element.html',1,'ChimeraTK']]],
