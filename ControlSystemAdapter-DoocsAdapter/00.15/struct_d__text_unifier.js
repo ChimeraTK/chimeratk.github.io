fb8f2e67cef524ccac263ab935374e8a730a36f9
Automated MSK Jenkins User <msk_jenkins@msk-ubuntu1604.desy.de>
2019-12-13 04:55:36 +0000

    Created/updated docu for ControlSystemAdapter-DoocsAdapter 00.15.03 (automatic commit)

--- ControlSystemAdapter-DoocsAdapter/00.15/struct_d__text_unifier.js
+++ ControlSystemAdapter-DoocsAdapter/00.15/struct_d__text_unifier.js
@@ -2,5 +2,5 @@ var struct_d__text_unifier =
 [
     [ "D_textUnifier", "struct_d__text_unifier.html#af1e3236f29a3ae88db7603f8bd8b9f0e", null ],
     [ "get_histPointer", "struct_d__text_unifier.html#a448a1633eb29a9158c5c26d23123e5f1", null ],
-    [ "set_and_archive", "struct_d__text_unifier.html#a3b165c1460790c41164a5dcf27e3f47e", null ]
+    [ "set_and_archive", "struct_d__text_unifier.html#a8763e1a831ab52a6ebd82a01a6a19113", null ]
 ];
