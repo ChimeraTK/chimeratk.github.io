a854c98ee7fd06ab5c996915ce7d4de26cf8b84f
Automated MSK Jenkins User <msk_jenkins@msk-ubuntu1604.desy.de>
2020-02-07 04:53:18 +0000

    Created/updated docu for DeviceAccess-DoocsBackend 00.19.02 (automatic commit)

--- DeviceAccess-DoocsBackend/00.19/search/all_12.js
+++ DeviceAccess-DoocsBackend/00.19/search/all_12.js
@@ -1,6 +1,11 @@
 var searchData=
 [
-  ['zmqsubscriptionmanager',['ZMQSubscriptionManager',['../class_chimera_t_k_1_1_doocs_backend_namespace_1_1_z_m_q_subscription_manager.html',1,'ChimeraTK::DoocsBackendNamespace']]],
-  ['zmqsubscriptionmanager_2ecc',['ZMQSubscriptionManager.cc',['../_z_m_q_subscription_manager_8cc.html',1,'']]],
-  ['zmqsubscriptionmanager_2eh',['ZMQSubscriptionManager.h',['../_z_m_q_subscription_manager_8h.html',1,'']]]
+  ['_7edoocsbackend',['~DoocsBackend',['../class_chimera_t_k_1_1_doocs_backend.html#a67efbbe747ea5719410305fb82065394',1,'ChimeraTK::DoocsBackend']]],
+  ['_7edoocsbackendfloatregisteraccessor',['~DoocsBackendFloatRegisterAccessor',['../class_chimera_t_k_1_1_doocs_backend_float_register_accessor.html#a38cae336ebbbabc3bdd5ba9336b2c0a3',1,'ChimeraTK::DoocsBackendFloatRegisterAccessor']]],
+  ['_7edoocsbackendifffregisteraccessor',['~DoocsBackendIFFFRegisterAccessor',['../class_chimera_t_k_1_1_doocs_backend_i_f_f_f_register_accessor.html#ad892c083c1bc80fae1b744df6c2b3869',1,'ChimeraTK::DoocsBackendIFFFRegisterAccessor']]],
+  ['_7edoocsbackendintregisteraccessor',['~DoocsBackendIntRegisterAccessor',['../class_chimera_t_k_1_1_doocs_backend_int_register_accessor.html#a84f7ad4d6b12628db73fb3bf02f553b1',1,'ChimeraTK::DoocsBackendIntRegisterAccessor']]],
+  ['_7edoocsbackendlongregisteraccessor',['~DoocsBackendLongRegisterAccessor',['../class_chimera_t_k_1_1_doocs_backend_long_register_accessor.html#a1f34bea6b68c766a9b357f1029b6d4c9',1,'ChimeraTK::DoocsBackendLongRegisterAccessor']]],
+  ['_7edoocsbackendregisteraccessor',['~DoocsBackendRegisterAccessor',['../class_chimera_t_k_1_1_doocs_backend_register_accessor.html#ab883217e9320e37adbb48d172b119758',1,'ChimeraTK::DoocsBackendRegisterAccessor']]],
+  ['_7edoocsbackendregisterinfo',['~DoocsBackendRegisterInfo',['../class_doocs_backend_register_info.html#a1ac1eafbe3f7526a92624abd374c4703',1,'DoocsBackendRegisterInfo']]],
+  ['_7edoocsbackendstringregisteraccessor',['~DoocsBackendStringRegisterAccessor',['../class_chimera_t_k_1_1_doocs_backend_string_register_accessor.html#a3504885499f8753b6365f83bc3ab6cdd',1,'ChimeraTK::DoocsBackendStringRegisterAccessor']]]
 ];
--- DeviceAccess-DoocsBackend/00.19/search/all_e.js
+++ DeviceAccess-DoocsBackend/00.19/search/all_e.js
@@ -1,12 +1,4 @@
 var searchData=
 [
-  ['savecatalogue',['saveCatalogue',['../namespace_cache.html#a9bcd131aceee0b32a5b5d0d10de93dac',1,'Cache']]],
-  ['shutdown',['shutdown',['../class_chimera_t_k_1_1_doocs_backend_register_accessor.html#a14e6481ebb423ffab7c49078e87fe6cf',1,'ChimeraTK::DoocsBackendRegisterAccessor']]],
-  ['shutdowncalled',['shutdownCalled',['../class_chimera_t_k_1_1_doocs_backend_register_accessor_base.html#aa7b9d96704ee9583991689100f7a9e13',1,'ChimeraTK::DoocsBackendRegisterAccessorBase']]],
-  ['slashes',['slashes',['../namespacedetail.html#adab1d68bf0f127a195efab7d3a74cb62',1,'detail']]],
-  ['src',['src',['../class_chimera_t_k_1_1_doocs_backend_register_accessor_base.html#a47151e07ff8caa8288db819dab284037',1,'ChimeraTK::DoocsBackendRegisterAccessorBase']]],
-  ['stringutility_2ecc',['StringUtility.cc',['../_string_utility_8cc.html',1,'']]],
-  ['stringutility_2eh',['StringUtility.h',['../_string_utility_8h.html',1,'']]],
-  ['subscribe',['subscribe',['../class_chimera_t_k_1_1_doocs_backend_namespace_1_1_z_m_q_subscription_manager.html#afccea9af8b0253c7b05590bee7157828',1,'ChimeraTK::DoocsBackendNamespace::ZMQSubscriptionManager']]],
-  ['supported_20data_20types',['Supported Data Types',['../supported_types.html',1,'']]]
+  ['todo_20list',['Todo List',['../todo.html',1,'']]]
 ];
