44a05e7f2693efc9d5a2e6279a1d02667f5c44e8
Automated MSK Jenkins User <msk_jenkins@msk-ubuntu1604.desy.de>
2020-04-21 03:53:50 +0000

    Created/updated docu for ControlSystemAdapter master (automatic commit)

--- ControlSystemAdapter/master/class_chimera_t_k_1_1_bidirectional_process_array.js
+++ ControlSystemAdapter/master/class_chimera_t_k_1_1_bidirectional_process_array.js
@@ -2,7 +2,7 @@ var class_chimera_t_k_1_1_bidirectional_process_array =
 [
     [ "SharedPtr", "class_chimera_t_k_1_1_bidirectional_process_array.html#a2d8f0a97ee81132f0871e322a3a33af4", null ],
     [ "BidirectionalProcessArray", "class_chimera_t_k_1_1_bidirectional_process_array.html#ab9d9a0fc5aeaca4b6557d9c2415cb39f", null ],
-    [ "dataValidity", "class_chimera_t_k_1_1_bidirectional_process_array.html#af53b8f4665e7fae0b0fba3ba14df5277", null ],
+    [ "dataValidity", "class_chimera_t_k_1_1_bidirectional_process_array.html#a0d22dcf75cce74be161b496b9ae1a597", null ],
     [ "doPostRead", "class_chimera_t_k_1_1_bidirectional_process_array.html#ac32a7a74262a70bd0dfe88401e2f6209", null ],
     [ "doPostWrite", "class_chimera_t_k_1_1_bidirectional_process_array.html#ab148110214143163be35496904a267f3", null ],
     [ "doPreRead", "class_chimera_t_k_1_1_bidirectional_process_array.html#a20a2e497a0cef7f629ecc50e1d2c0d7a", null ],
@@ -15,7 +15,7 @@ var class_chimera_t_k_1_1_bidirectional_process_array =
     [ "getUniqueId", "class_chimera_t_k_1_1_bidirectional_process_array.html#a84b3d639a532053b86710d3ca2104192", null ],
     [ "getVersionNumber", "class_chimera_t_k_1_1_bidirectional_process_array.html#a9a54507fdf2344c0ee6c16bafc6de533", null ],
     [ "interrupt", "class_chimera_t_k_1_1_bidirectional_process_array.html#abe253acf5011ef0fe61a288c9050171f", null ],
-    [ "setDataValidity", "class_chimera_t_k_1_1_bidirectional_process_array.html#ae14fd1d0a84cbcc45d25ac73cc0ee07b", null ],
+    [ "setDataValidity", "class_chimera_t_k_1_1_bidirectional_process_array.html#a5fff00020d25289a6750b589e6f29ddd", null ],
     [ "setPersistentDataStorage", "class_chimera_t_k_1_1_bidirectional_process_array.html#ae9d55a715a86e0c0ff4cb4380b90f569", null ],
     [ "setValueRejectCallback", "class_chimera_t_k_1_1_bidirectional_process_array.html#a9ad4005d5d301a2befaa2120e021590d", null ],
     [ "createBidirectionalSynchronizedProcessArray", "class_chimera_t_k_1_1_bidirectional_process_array.html#ae4f47b064666200ab0a8ef3b6e8d4979", null ],
--- ControlSystemAdapter/master/class_chimera_t_k_1_1_unidirectional_process_array.js
+++ ControlSystemAdapter/master/class_chimera_t_k_1_1_unidirectional_process_array.js
@@ -3,7 +3,7 @@ var class_chimera_t_k_1_1_unidirectional_process_array =
     [ "SharedPtr", "class_chimera_t_k_1_1_unidirectional_process_array.html#ab0fb62fab7a4974cd6f9f096902f4449", null ],
     [ "UnidirectionalProcessArray", "class_chimera_t_k_1_1_unidirectional_process_array.html#a3ad69ac68b662e3d11f24f8c006fe80b", null ],
     [ "UnidirectionalProcessArray", "class_chimera_t_k_1_1_unidirectional_process_array.html#abdd6892ad2f837b34b680b0e0a2dc8a7", null ],
-    [ "dataValidity", "class_chimera_t_k_1_1_unidirectional_process_array.html#a61f309a1d2dcc9315f5916a373420697", null ],
+    [ "dataValidity", "class_chimera_t_k_1_1_unidirectional_process_array.html#a5f74e11410915dc04e1eb3fba114d247", null ],
     [ "doPostRead", "class_chimera_t_k_1_1_unidirectional_process_array.html#a5793b57ee2a16dbfafcb8f0b28519561", null ],
     [ "doReadTransfer", "class_chimera_t_k_1_1_unidirectional_process_array.html#a4c10cec4751c5bb4370bdc61e13654cc", null ],
     [ "doReadTransferAsync", "class_chimera_t_k_1_1_unidirectional_process_array.html#a64cfeee87162fa14fe7474c3dadf96d4", null ],
@@ -14,7 +14,7 @@ var class_chimera_t_k_1_1_unidirectional_process_array =
     [ "getUniqueId", "class_chimera_t_k_1_1_unidirectional_process_array.html#a3cfa68f1632196e2e1c093c1e7e3c5f8", null ],
     [ "getVersionNumber", "class_chimera_t_k_1_1_unidirectional_process_array.html#a57a5a9bacf608a411500dccf43c39e55", null ],
     [ "interrupt", "class_chimera_t_k_1_1_unidirectional_process_array.html#ac48b0c941a29ad8a1e0a04f24277b46e", null ],
-    [ "setDataValidity", "class_chimera_t_k_1_1_unidirectional_process_array.html#a73129880919f49b432f339b1c3c3f816", null ],
+    [ "setDataValidity", "class_chimera_t_k_1_1_unidirectional_process_array.html#adea4da9c702cdae16e7ab32876430442", null ],
     [ "setPersistentDataStorage", "class_chimera_t_k_1_1_unidirectional_process_array.html#a70445d795d2a69b6214d0f5d5738c3e6", null ],
     [ "createSynchronizedProcessArray", "class_chimera_t_k_1_1_unidirectional_process_array.html#ae375549accb12aa3d9c3fef87e122eba", null ],
     [ "createSynchronizedProcessArray", "class_chimera_t_k_1_1_unidirectional_process_array.html#a68ad426f404d174aa747336bba32126a", null ]
