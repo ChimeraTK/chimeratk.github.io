611b7275a8a75c9207171db3a3d695c196cfa65f
Automated MSK Jenkins User <msk_jenkins@msk-ubuntu1604.desy.de>
2020-09-30 14:04:33 +0000

    Created/updated docu for ControlSystemAdapter-DoocsAdapter 01.00.00 (automatic commit)

---
+++ ControlSystemAdapter-DoocsAdapter/01.00/class_chimera_t_k_1_1_doocs_updater.js
@@ -0,0 +1,13 @@
+var class_chimera_t_k_1_1_doocs_updater =
+[
+    [ "ToDoocsUpdateDescriptor", "struct_chimera_t_k_1_1_doocs_updater_1_1_to_doocs_update_descriptor.html", "struct_chimera_t_k_1_1_doocs_updater_1_1_to_doocs_update_descriptor" ],
+    [ "~DoocsUpdater", "class_chimera_t_k_1_1_doocs_updater.html#a59282eb42563fb3483756170bda65329", null ],
+    [ "addVariable", "class_chimera_t_k_1_1_doocs_updater.html#a90b9cdeafcf5e2ceacef04ecbd891b81", null ],
+    [ "run", "class_chimera_t_k_1_1_doocs_updater.html#aafe7082f09c25245074fed216d78c528", null ],
+    [ "stop", "class_chimera_t_k_1_1_doocs_updater.html#ae95813ee7f671133d514189c3c033286", null ],
+    [ "update", "class_chimera_t_k_1_1_doocs_updater.html#a47e2fd420a881a64e0ab38d4b4bba5aa", null ],
+    [ "updateLoop", "class_chimera_t_k_1_1_doocs_updater.html#a9fe9fd577b491a49f7205fd86ca754f7", null ],
+    [ "_elementsToRead", "class_chimera_t_k_1_1_doocs_updater.html#abe9102d68e37adc3bf7d2d484ccbcfea", null ],
+    [ "_syncThread", "class_chimera_t_k_1_1_doocs_updater.html#aa702766800d6c1863c232758f723798e", null ],
+    [ "_toDoocsDescriptorMap", "class_chimera_t_k_1_1_doocs_updater.html#a1603c55d5a148d4e2afdf95e2f0730d2", null ]
+];
---
+++ ControlSystemAdapter-DoocsAdapter/01.00/search/variables_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['additionaltransferelements',['additionalTransferElements',['../struct_chimera_t_k_1_1_doocs_updater_1_1_to_doocs_update_descriptor.html#a8182c0fda0b79779b330f52f1c220084',1,'ChimeraTK::DoocsUpdater::ToDoocsUpdateDescriptor']]],
+  ['axis',['axis',['../struct_chimera_t_k_1_1_xy_description.html#a38586b96103e0eedbd8eef5475620170',1,'ChimeraTK::XyDescription']]]
+];
