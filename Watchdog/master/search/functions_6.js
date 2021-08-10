7ea383e9cc83a1c13b5057427f92e622efc172a6
Automated MSK Jenkins User <msk_jenkins@msk-ubuntu1604.desy.de>
2021-08-10 03:59:19 +0000

    Created/updated docu for Watchdog master (automatic commit)

--- Watchdog/master/search/functions_6.js
+++ Watchdog/master/search/functions_6.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['logfilegroup',['LogFileGroup',['../structLogFileModule_1_1LogFileGroup.html#ae3b612a1972f0c9c1021fa6578d68d16',1,'LogFileModule::LogFileGroup::LogFileGroup(EntityOwner *owner, const std::string &amp;pathToLogFile, const std::unordered_set&lt; std::string &gt; &amp;tags={})'],['../structLogFileModule_1_1LogFileGroup.html#aad1cc4d50c5aa37dc9c22f2dba48b9a4',1,'LogFileModule::LogFileGroup::LogFileGroup()'],['../structLoggingModule_1_1LogFileGroup.html#a9b81b25d2aa46c9055a4bba3f39de77b',1,'LoggingModule::LogFileGroup::LogFileGroup(EntityOwner *owner, const std::string &amp;pathToLogFile, const std::unordered_set&lt; std::string &gt; &amp;tags={})'],['../structLoggingModule_1_1LogFileGroup.html#a08cbb0a04118f877c33144cd90b2f37e',1,'LoggingModule::LogFileGroup::LogFileGroup()']]],
   ['logfilemodule',['LogFileModule',['../structLogFileModule.html#a4bf992259d951643f17e0bb411bc9756',1,'LogFileModule']]],
-  ['loggingmodule',['LoggingModule',['../structLoggingModule.html#a54294cd517646051efc6564b1f47df5c',1,'LoggingModule']]]
+  ['loggingmodule',['LoggingModule',['../structLoggingModule.html#ae4ca26b827c555c4a65a0fead0453460',1,'LoggingModule']]]
 ];
