d3b20824735620ac609eeb7fba2c688faed9eb2e
Automated MSK Jenkins User <msk_jenkins@msk-ubuntu1604.desy.de>
2022-03-14 15:31:35 +0000

    Created/updated docu for ControlSystemAdapter master (automatic commit)

--- ControlSystemAdapter/master/struct_process_array_factory_backend.js
+++ ControlSystemAdapter/master/struct_process_array_factory_backend.js
@@ -2,11 +2,12 @@ var struct_process_array_factory_backend =
 [
     [ "BackendRegisterer", "class_process_array_factory_backend_1_1_backend_registerer.html", "class_process_array_factory_backend_1_1_backend_registerer" ],
     [ "ProcessArrayFactoryBackend", "struct_process_array_factory_backend.html#a2abef35a33451e6af8beef28b83af325", null ],
-    [ "~ProcessArrayFactoryBackend", "struct_process_array_factory_backend.html#a597d0619079c2d0a43fae6a6d88307aa", null ],
     [ "activateAsyncRead", "struct_process_array_factory_backend.html#aa6129ff54e695ea267948cc8ff17f496", null ],
     [ "close", "struct_process_array_factory_backend.html#aed60121d04d6a727050f459d9e55c11e", null ],
     [ "DEFINE_VIRTUAL_FUNCTION_TEMPLATE_VTABLE_FILLER", "struct_process_array_factory_backend.html#a6b6575befe721b7e5e79d62545d84257", null ],
+    [ "getMetadataCatalogue", "struct_process_array_factory_backend.html#a0bd651d037fbd925f4c3f340ae8cd024", null ],
     [ "getRegisterAccessor_impl", "struct_process_array_factory_backend.html#a74fd1b917c789bb4dd657d78843753ac", null ],
+    [ "getRegisterCatalogue", "struct_process_array_factory_backend.html#ad68a5639d7a070b18e5283c004356c4e", null ],
     [ "isFunctional", "struct_process_array_factory_backend.html#ac39250a2d8a6436b4619587abacd65db", null ],
     [ "open", "struct_process_array_factory_backend.html#a1127b3c71fef4f58e4895af0447fe170", null ],
     [ "readDeviceInfo", "struct_process_array_factory_backend.html#aa540d33dff5765b01dda6e6a5848324d", null ],
