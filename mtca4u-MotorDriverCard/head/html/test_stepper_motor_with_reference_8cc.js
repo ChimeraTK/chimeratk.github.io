5846f3bb4fe952bb0eb8fe48d284994d68df7f9e
Automated MSK Jenkins User <msk_jenkins@jenkins-vm>
2025-03-18 06:39:08 +0000

    Automated documentation update

--- mtca4u-MotorDriverCard/head/html/test_stepper_motor_with_reference_8cc.js
+++ mtca4u-MotorDriverCard/head/html/test_stepper_motor_with_reference_8cc.js
@@ -6,8 +6,5 @@ var test_stepper_motor_with_reference_8cc =
     [ "BOOST_AUTO_TEST_CASE", "test_stepper_motor_with_reference_8cc.html#a2f78bbfd6eac36fb84ef37079c499e08", null ],
     [ "BOOST_AUTO_TEST_CASE", "test_stepper_motor_with_reference_8cc.html#a028932b22bd596c67509ed83af7502f9", null ],
     [ "BOOST_AUTO_TEST_CASE", "test_stepper_motor_with_reference_8cc.html#a005884d3e124731d33da745253c953e0", null ],
-    [ "BOOST_AUTO_TEST_CASE", "test_stepper_motor_with_reference_8cc.html#a4db1789457db5d6d9da9b534a7022842", null ],
-    [ "BOOST_AUTO_TEST_CASE", "test_stepper_motor_with_reference_8cc.html#a871aaac844c361cfd52b123ee4fca895", null ],
-    [ "BOOST_AUTO_TEST_CASE", "test_stepper_motor_with_reference_8cc.html#a3e79950474e2315a4f13f10d2d70e646", null ],
     [ "BOOST_AUTO_TEST_CASE", "test_stepper_motor_with_reference_8cc.html#a07f09699aedde03b5910e908459dcbf7", null ]
 ];
