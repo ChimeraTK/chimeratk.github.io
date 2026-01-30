4037f97ffdeab15ba894352cffef3be5d319b75c
Automated MSK Jenkins User <msk_jenkins@jenkins-vm>
2026-01-30 06:39:18 +0000

    Automated documentation update

--- ChimeraTK-ControlSystemAdapter-DoocsAdapter/head/html/search/functions_15.js
+++ ChimeraTK-ControlSystemAdapter-DoocsAdapter/head/html/search/functions_15.js
@@ -2,8 +2,9 @@ var searchData=
 [
   ['_7ecsadaptereqfct_0',['~CSAdapterEqFct',['../class_chimera_t_k_1_1_c_s_adapter_eq_fct.html#a0dec25c90e83446eaa064fcc4a2b4bdb',1,'ChimeraTK::CSAdapterEqFct']]],
   ['_7edoocsupdater_1',['~DoocsUpdater',['../class_chimera_t_k_1_1_doocs_updater.html#a59282eb42563fb3483756170bda65329',1,'ChimeraTK::DoocsUpdater']]],
-  ['_7eglobalfixture_2',['~GlobalFixture',['../struct_global_fixture.html#a7368726e829abaed893d3e9f8007e920',1,'GlobalFixture']]],
-  ['_7epropertybase_3',['~PropertyBase',['../class_chimera_t_k_1_1_property_base.html#a5023202fa5df70b195677491547a8ca2',1,'ChimeraTK::PropertyBase']]],
-  ['_7epropertydescription_4',['~PropertyDescription',['../struct_chimera_t_k_1_1_property_description.html#a5b0765eb3a876c9ac52adf19552e52ac',1,'ChimeraTK::PropertyDescription']]],
-  ['_7ezeromqfixture_5',['~ZeroMQFixture',['../struct_zero_m_q_fixture.html#aa55dd3c585f8f00f367964a7b107e561',1,'ZeroMQFixture']]]
+  ['_7eextendedtestapplication_2',['~ExtendedTestApplication',['../struct_extended_test_application.html#a536289eae66a317db19bd3366574bc23',1,'ExtendedTestApplication']]],
+  ['_7eglobalfixture_3',['~GlobalFixture',['../struct_global_fixture.html#a7368726e829abaed893d3e9f8007e920',1,'GlobalFixture']]],
+  ['_7epropertybase_4',['~PropertyBase',['../class_chimera_t_k_1_1_property_base.html#a5023202fa5df70b195677491547a8ca2',1,'ChimeraTK::PropertyBase']]],
+  ['_7epropertydescription_5',['~PropertyDescription',['../struct_chimera_t_k_1_1_property_description.html#a5b0765eb3a876c9ac52adf19552e52ac',1,'ChimeraTK::PropertyDescription']]],
+  ['_7ezeromqfixture_6',['~ZeroMQFixture',['../struct_zero_m_q_fixture.html#aa55dd3c585f8f00f367964a7b107e561',1,'ZeroMQFixture']]]
 ];
